6852498ad7df1e4e49585a31d9bc671d86729777
NBKeerthana <70811915+NBKeerthana@users.noreply.github.com>
2021-07-27 18:00:35 +0530

    Update Block.js

--- Block.js
+++ Block.js
@@ -8,12 +8,14 @@ class Block{
       this.body = Bodies.rectangle(x, y, width, height, options);
       this.width = width;
       this.height = height;
+      this.visibility = 255;
       
       World.add(world, this.body);
     }
     display(){
       var pos =this.body.position;
       var angle = this.body.angle;
+      if(this.body.speed<3){
       push();
       translate(pos.x, pos.y);
       rotate(angle);
@@ -23,5 +25,15 @@ class Block{
       fill(255);
       rect(0, 0, this.width, this.height);
       pop();
+      }
+      else{
+        push ();
+         World.remove(world,this.body); 
+         this.visiblity = this.visiblity-5
+         tint(255,this.visiblity) 
+         rect(0,0,this.width,this.height); 
+         pop();
+      }
+      
     }
   }
